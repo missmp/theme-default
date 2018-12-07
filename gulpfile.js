1bb01b16579f3a2022a7f2914da6c7ebd8ee1c74
danhtran94 <dd2672015@gmail.com>
2018-12-07 14:24:30 +0700

    reconfig gulp watch & custom based on branch style

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,39 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var cssmin = require('gulp-cssmin');
-var salad = require('postcss-salad')(require('./salad.config.json'));
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var postcss = require("gulp-postcss");
+// var cssmin = require("gulp-cssmin");
+var salad = require("postcss-salad")(require("./salad.config.json"));
 
-gulp.task('compile', function() {
-  return gulp.src('./src/*.css')
-    .pipe(postcss([salad]))
-    .pipe(cssmin())
-    .pipe(gulp.dest('./lib'));
+gulp.task("compile", function() {
+  return (
+    gulp
+      .src("./src/index.css")
+      .pipe(postcss([salad]))
+      // .pipe(cssmin())
+      .pipe(gulp.dest("../element"))
+  );
 });
 
-gulp.task('copyfont', function() {
-  return gulp.src('./src/fonts/**')
-    .pipe(cssmin())
-    .pipe(gulp.dest('./lib/fonts'));
+gulp.task("callback", function() {
+  // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+  return watch("./src/*.css", { verbose: true }, function() {
+    gulp
+      .src("./src/*.css")
+      .pipe(postcss([salad]))
+      // .pipe(cssmin())
+      .pipe(gulp.dest("../element"));
+  });
 });
 
-gulp.task('build', ['compile', 'copyfont']);
+gulp.task("copyfont", function() {
+  return (
+    gulp
+      .src("./src/fonts/**")
+      // .pipe(cssmin())
+      .pipe(gulp.dest("../element/fonts"))
+  );
+});
+
+gulp.task("build", ["compile", "copyfont"]);
